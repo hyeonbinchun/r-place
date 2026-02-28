6166398c4c33944ea6af9668beca02ea986bb998
owen <owen2781379@gmail.com>
2026-02-28 03:19:45 -0500

    [FIX] doit.bash

--- place_v3/doit.bash
+++ place_v3/doit.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-# docker swarm init --advertise-addr 192.168.1.218
-# docker swarm join --token SWMTKN-1-12d4fhqyyji827haw9qw3iniee3xac3yy59mz2t9v8enlenwha-6rtn2dklkfprqq3eh4d9wijrp 192.168.1.218:2377
+# docker swarm init --advertise-addr 192.168.1.XXX
+# docker swarm join --token XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX 192.168.1.XXX:2377
 docker stack deploy -c docker-compose.yml placestack
 
